18950a3f5741c0d41ab371019e1610d71ad38316
Pablo <pablo@adimedia.net>
2025-12-11 15:19:52 +0100

    Spring init

---
+++ src/main/java/com/plaiaundi/sepe/seid/seid/SeidApplication.java
@@ -0,0 +1,13 @@
+package com.plaiaundi.sepe.seid.seid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plaiaundi/sepe/seid/seid/SeidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plaiaundi.sepe.seid.seid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
