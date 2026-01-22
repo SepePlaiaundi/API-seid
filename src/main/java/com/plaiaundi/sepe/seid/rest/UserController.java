a01476aaac70fdb9fe36ab95dde5e57a169e37ad
ikebg-salmaneaaboud <ikebg@plaiaundi.net>
2026-01-22 12:34:24 +0100

    agregación del dto de UserResponse, así como el endpoint de /users/all

--- src/main/java/com/plaiaundi/sepe/seid/rest/UserController.java
+++ src/main/java/com/plaiaundi/sepe/seid/rest/UserController.java
@@ -1,23 +1,31 @@
 package com.plaiaundi.sepe.seid.rest;
 
+import com.plaiaundi.sepe.seid.dominio.dao.CameraRepository;
+import com.plaiaundi.sepe.seid.dominio.dao.UserRepository;
+import com.plaiaundi.sepe.seid.dominio.model.User;
 import com.plaiaundi.sepe.seid.dominio.services.UserService;
-import com.plaiaundi.sepe.seid.dto.LoginResponse;
-import com.plaiaundi.sepe.seid.dto.UserLoginRequest;
-import com.plaiaundi.sepe.seid.dto.UserRegisterRequest;
+import com.plaiaundi.sepe.seid.dto.*;
 import com.plaiaundi.sepe.seid.infrastructure.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
@@ -55,4 +63,10 @@ public class UserController {
         }
     }
 
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserResponse> list() {
+        return userRepository.findAll().stream()
+                .map(user -> new UserResponse(user.getNombreCompleto(), user.getEmail()))
+                .toList();
+    }
 }
