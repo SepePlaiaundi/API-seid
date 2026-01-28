8b7206fda6361056d1db3e00f2b10eed6cfb636a
Pablo <pablo@adimedia.net>
2026-01-28 16:41:35 +0100

    Landing page

--- src/main/java/com/plaiaundi/sepe/seid/rest/Documentation.java
+++ src/main/java/com/plaiaundi/sepe/seid/rest/Documentation.java
@@ -3,14 +3,12 @@ package com.plaiaundi.sepe.seid.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class Documentation {
-    
-    @GetMapping({"","/"})
-    public String wellcome() {
-        return "redirect:/swagger-ui/index.html";
+
+    @GetMapping({ "", "/" })
+    public String welcome() {
+        return "redirect:/index.html";
     }
-    
 
 }
